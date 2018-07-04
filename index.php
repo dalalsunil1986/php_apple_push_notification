927ec438910abeb1cc4de3df7488d6bbb164935a
Ronit Mukherjee <connect.ronit@gmail.com>
2018-07-04 12:59:24 +0530

    File name updated

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once "lib/ApplePushNotification.php";
-use apple_push_notification\ApplePushNotification;
-
-$APNObj = new ApplePushNotification("","DEVCertificates.pem");
-
-$APNObj->sendPush("5d3ebd9945f886598bd8c5f05d6fa8951997b9268dc6eb9e91c9a9d21bc1bb65","This is a test message",array("event_id"=>1));
